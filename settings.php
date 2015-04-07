abcff5527a7d4380ba4563741277f62ebfde2848
pantobp <nemanjapantovic@live.ac.me>
2015-04-07 14:24:32 +0200

    First commit

---
+++ settings.php
@@ -0,0 +1,26 @@
+<?php
+	$adresa[0]="32 Via Mizner, Worth Ave.​"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad[0]="Palm Beach, Florida 33480";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica[0]=""; // AKo ne znate skracenicu ostavite praznu
+	$nazivlokacije[0]="Clorinda Antinori Palm Beach, Florida";
+	$telephone[0]=2392277076;// Broj koji se poziva na dugme CALL US
+
+	$adresa[1]="Third Street South​"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad[1]="Naples, Florida 34102";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica[1]=""; // AKo ne znate skracenicu ostavite praznu
+	$nazivlokacije[1]="Valentina Boutique in Naples, Florida";
+	$telephone[1]=2399194217;// Broj koji se poziva na dugme CALL US
+
+	$adresa[2]="32-34 Borgo Ognissanti"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad[2]="Florence, Italy 50123";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica[2]=""; // AKo ne znate skracenicu ostavite praznu
+	$nazivlokacije[2]="Clorinda Antinori Florence, Italy";
+	$telephone[2]= +393890480084;// Broj koji se poziva na dugme CALL US
+
+
+
+	$centratelephone=2399194217;
+	$companynumber=3;
+	$theme="b";//Thema koja se aplicira na sve
+	$transitionefect="flip"; //kucate tranziciju koju zelite na stranama . Vrste tranzicija : "fade", "flip", "pop", "slide", "slidedown", "slideup"
+?>
